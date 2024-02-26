e1aadd4035da3b31cc5ee46b091709e12c0672ee
MacOS Monterey <macosmonterey@mac-pro-de-big.home>
2024-02-26 16:35:09 +0100

    ajout message erreur

--- scripts/inputSearch.js
+++ scripts/inputSearch.js
@@ -1,6 +1,6 @@
 import { recipes } from "../data/recipes.js";
 
-const allRecipes = recipes.slice();
+const allRecipes = recipes;
 
 function filterRecipes(searchTerm) {
     return allRecipes.filter(recipe => {
@@ -13,14 +13,24 @@ function filterRecipes(searchTerm) {
 }
 
 function searchFilterMethod(displayRecipes) {
+    const inputSearch = document.getElementById('input-search');
+    const errorSearch = document.getElementById('error-search');
+
     inputSearch.addEventListener('input', (event) => {
         const searchTerm = event.target.value.trim().toLowerCase();
 
         if (searchTerm.length >= 3) {
             const filteredRecipes = filterRecipes(searchTerm);
             displayRecipes(filteredRecipes);
+
+            if (filteredRecipes.length > 0) {
+                errorSearch.style.display = 'none';
+            } else {
+                errorSearch.style.display = 'block';
+            }
         } else {
             displayRecipes(allRecipes);
+            errorSearch.style.display = 'none';
         }
     });
 }
